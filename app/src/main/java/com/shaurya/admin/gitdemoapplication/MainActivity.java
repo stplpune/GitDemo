759b5e69a86d682d68d7cbb0db30c7c6f16c8edc
Shaurya Technosoft <38429228+stplpune@users.noreply.github.com>
2018-07-18 12:32:36 +0530

    commit amol

--- app/src/main/java/com/shaurya/admin/gitdemoapplication/MainActivity.java
+++ app/src/main/java/com/shaurya/admin/gitdemoapplication/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action amol", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Set your action Name", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
                 //Amrut test git hub for first time
             }
